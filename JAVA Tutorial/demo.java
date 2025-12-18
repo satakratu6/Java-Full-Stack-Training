4fd6bc5d9e0a16a2eb1cde39c93f6e168c9cb71c
Satakratu Chakraborty <satakratu.madhu@gmail.com>
2025-12-18 11:53:58 +0530

    first day

---
+++ JAVA Tutorial/demo.java
@@ -0,0 +1,30 @@
+class demo{
+    public static void main(String[] args) {
+        // System.out.print(10+" ");
+        // System.out.print(20+" ");
+        // System.out.println(30);
+        // System.out.println(40);
+        // System.out.println(50);
+        // System.out.print(60 +" ");
+        // System.out.print(70+" ");
+        // System.out.println(80);
+        // System.out.println(90);
+        // System.out.print(100+" ");
+        // System.out.println(110);
+        // System.out.print(120);
+        System.out.print(5+" ");
+        System.out.print(25+" ");
+        System.out.print(75+" ");
+        System.out.println(85);
+        System.out.print(25+" ");
+        System.out.print(45+" ");
+        System.out.println(35);
+        System.out.print(15+" ");
+        System.out.println(20);
+        System.out.println(5);
+        System.out.println(55);
+        System.out.print(66+" ");
+        System.out.print(67);
+        
+    }
+}
